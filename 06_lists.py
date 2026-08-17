978f21bfa17399b01867e18a95f2be3c4fe612b5
alnahid2003-del <al.nahid2003@gmail.com>
2026-08-18 01:36:07 +0600

    Update 06_lists.py

--- 06_lists.py
+++ 06_lists.py
@@ -1,1 +1,28 @@
+# Python Lists
 
+tasks = [
+    "Search Google",
+    "Read a file",
+    "Calculate result",
+    "Send email"
+]
+
+print("All Tasks:")
+print(tasks)
+
+print("\nFirst Task:")
+print(tasks[0])
+
+print("\nNumber of Tasks:")
+print(len(tasks))
+
+# Add a new task
+tasks.append("Call API")
+
+print("\nAfter Adding New Task:")
+print(tasks)
+
+# Loop through tasks
+print("\nTask List:")
+for task in tasks:
+    print("-", task)
