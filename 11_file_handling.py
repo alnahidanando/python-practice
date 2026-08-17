b56a487d43d749774e3a8c88504d2e7f79a3fa57
alnahid2003-del <al.nahid2003@gmail.com>
2026-08-18 01:55:02 +0600

    Update 11_file_handling.py

--- 11_file_handling.py
+++ 11_file_handling.py
@@ -1,1 +1,40 @@
+# Write to a file
 
+file = open("notes.txt", "w")
+file.write("Python is the foundation of AI development.\n")
+file.write("I am learning Python to build AI Agents.")
+file.close()
+
+
+# Read from a file
+
+file = open("notes.txt", "r")
+content = file.read()
+print("File content:")
+print(content)
+file.close()
+
+
+# Append to a file
+
+file = open("notes.txt", "a")
+file.write("\nI will learn LLMs, RAG and AI Agents.")
+file.close()
+
+
+# Read line by line
+
+file = open("notes.txt", "r")
+
+for line in file:
+    print("Line:", line.strip())
+
+file.close()
+
+
+# Using with statement
+
+with open("notes.txt", "r") as file:
+    content = file.read()
+    print("Using with:")
+    print(content)
