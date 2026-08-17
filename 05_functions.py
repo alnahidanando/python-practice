de5a8cac07896b2122eafa22b3bf2f0a74261db0
alnahid2003-del <al.nahid2003@gmail.com>
2026-08-18 01:34:57 +0600

    Update 05_functions.py

--- 05_functions.py
+++ 05_functions.py
@@ -1,1 +1,19 @@
+def greet(name):
+    print("Hello,", name)
 
+
+def add(a, b):
+    return a + b
+
+
+def calculate_total(price, quantity):
+    return price * quantity
+
+
+greet("Al Nahid")
+
+result = add(10, 20)
+print("Sum:", result)
+
+total = calculate_total(500, 3)
+print("Total price:", total)
