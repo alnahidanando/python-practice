4561002bad781f36d54175af92880a066a87ac4a
alnahid2003-del <al.nahid2003@gmail.com>
2026-08-18 01:50:47 +0600

    Update 09_strings.py

--- 09_strings.py
+++ 09_strings.py
@@ -1,1 +1,30 @@
+name = "Al Nahid"
+message = "I am learning Python for AI Agents"
 
+print("Name:", name)
+print("Message:", message)
+
+print("First character:", name[0])
+print("Last character:", name[-1])
+
+print("First 7 characters:", name[:7])
+print("From character 3:", name[3:])
+
+print("Uppercase:", name.upper())
+print("Lowercase:", name.lower())
+
+text = "   Python is powerful   "
+print("Without spaces:", text.strip())
+
+sentence = "Python is easy to learn"
+words = sentence.split()
+print("Words:", words)
+
+joined_text = "-".join(words)
+print("Joined:", joined_text)
+
+new_sentence = sentence.replace("easy", "fun")
+print("Changed:", new_sentence)
+
+age = 22
+print(f"My name is {name} and I am {age} years old.")
